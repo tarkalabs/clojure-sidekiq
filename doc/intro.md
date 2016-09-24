a6553598e37b59701eaac27b51f1e1796856d313
Deepak R <deepak@tarkalabs.com>
2016-09-24 20:09:30 +0530

    clojure-sidekiq is born.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-sidekiq
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_sidekiq/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-sidekiq.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-sidekiq.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
