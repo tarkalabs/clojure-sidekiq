01b1fc7f1347d98bb150584aa74ad2f8c523652c
Deepak R <deepak@tarkalabs.com>
2016-09-29 19:15:01 +0530

    More tests added.

--- test/clojure_sidekiq/core_test.clj
+++ test/clojure_sidekiq/core_test.clj
@@ -4,13 +4,22 @@
             [clojure.data.json :as json]
             [clojure-sidekiq.core :refer :all]))
 
-
 (defn payload-attributes [payload-string]
   (let [payload (json/read-str payload-string)]
-    (select-keys payload ["class" "args"])))
+    (select-keys payload ["class" "args" "retry"])))
 
 (deftest enqueue-defaults
+  (wcar* (car/flushall))
   (let [worker-name "TestWorker"
         args [1 2 3]]
-    (enqueue "TestWorker" [1 2 3])
-    (is (= (payload-attributes (wcar* (car/lpop "queue:default"))) {"class" worker-name"args" args}))))
+    (enqueue worker-name args)
+    (is (= (payload-attributes (wcar* (car/lpop "queue:default"))) {"class" worker-name "args" args "retry" false})))
+    (is (= (wcar* (car/smembers "queues") ["default"]))))
+
+(deftest enqueue-non-defaults
+  (wcar* (car/flushall))
+  (let [worker-name "AnotherWorker"
+        args ["some-string-arg"]]
+    (enqueue worker-name args {:queue "my-queue" :retry true})
+    (is (= (payload-attributes (wcar* (car/lpop "queue:my-queue"))) {"class" worker-name "args" args "retry" true})))
+    (is (= (wcar* (car/smembers "queues") ["my-queue"]))))
